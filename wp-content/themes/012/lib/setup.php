0ddd88a636cdb5df7ff821364dc3c42af55f8aae
Jeff Jenkinson <jeff.jenkinson@gmail.com>
2015-10-19 22:13:15 +0200

    Starting on footer and adding an instagram feed.

--- wp-content/themes/012/lib/setup.php
+++ wp-content/themes/012/lib/setup.php
@@ -106,12 +106,30 @@ function widgets_init() {
   ]);
 
   register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
+    'name'          => __('Footer Instagram', 'sage'),
+    'id'            => 'sidebar-footer-instagram',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h3>',
     'after_title'   => '</h3>'
+  ]);
+
+  register_sidebar([
+      'name'          => __('Footer Twitter', 'sage'),
+      'id'            => 'sidebar-footer-twitter',
+      'before_widget' => '<section class="widget %1$s %2$s">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>'
+  ]);
+
+  register_sidebar([
+      'name'          => __('Footer Newsletter', 'sage'),
+      'id'            => 'sidebar-footer-newsletter',
+      'before_widget' => '<section class="widget %1$s %2$s">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>'
   ]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
--- wp-content/themes/012/templates/footer.php
+++ wp-content/themes/012/templates/footer.php
@@ -1,5 +1,18 @@
-<footer class="content-info">
+<footer class="container-fluid">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="title">Connect</h1>
+        <?php dynamic_sidebar('sidebar-footer-instagram'); ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-5">
+        <?php dynamic_sidebar('sidebar-footer-twitter'); ?>
+      </div>
+      <div class="col-md-7">
+        <?php dynamic_sidebar('sidebar-footer-newsletter'); ?>
+      </div>
+    </div>
   </div>
 </footer>
