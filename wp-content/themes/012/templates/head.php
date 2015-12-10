a66cbfc3a69243fe12978f68610c03bf7c2a53cf
Jeff Jenkinson <jeff.jenkinson@gmail.com>
2015-12-10 21:21:46 +0200

    Moving over to Nicework typekit.

--- wp-content/themes/012/templates/head.php
+++ wp-content/themes/012/templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="https://use.typekit.net/tfd0ufz.js"></script>
+  <script src="https://use.typekit.net/ski8dpz.js"></script>
+  <script>try{Typekit.load({ async: true });}catch(e){}</script>
   <?php wp_head(); ?>
 </head>
